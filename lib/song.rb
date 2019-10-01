62b586ba9ab9b89532d63daf5862c5cc7f05275a
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-10-01 20:00:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
-        artist_count[artist] + 1 
+        artist_count[artist] += 1 
       else
         artist_count[artist] = 1 
       end
